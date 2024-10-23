0cf8da08a6e7a7d07cbd05fc20e44e5593dfe289
Devesh-code <developersharma1999@gmail.com>
2024-10-23 17:11:57 +0530

    working on sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from "./components/navbar"
+import Sidebar from './components/sidebar'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Sidebar/>
     </>
   )
 }
---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { HiHome } from "react-icons/hi2";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { BiSolidUserAccount } from "react-icons/bi";
+import { TbHistory } from "react-icons/tb";
+import { RiPlayList2Fill } from "react-icons/ri";
+import { PiVideo } from "react-icons/pi";
+import { LuClock3 } from "react-icons/lu";
+import { SlLike } from "react-icons/sl";
+import { PiFireBold } from "react-icons/pi";
+import { PiShoppingCart } from "react-icons/pi";
+import { CgMusicNote } from "react-icons/cg";
+import { PiFilmSlateDuotone } from "react-icons/pi";
+import { TbBrandLivewire } from "react-icons/tb";
+import { LuGamepad2 } from "react-icons/lu";
+import { FaRegNewspaper } from "react-icons/fa6";
+import { GoTrophy } from "react-icons/go";
+import { CgBulb } from "react-icons/cg";
+import { PiDressLight } from "react-icons/pi";
+import { FaPodcast } from "react-icons/fa6";
+
+const sidebar = () => {
+  return (
+  <div>
+        <HiHome/>
+        <SiYoutubeshorts/>
+        <MdOutlineSubscriptions/>
+        <BiSolidUserAccount/>
+        <TbHistory/>    
+        <RiPlayList2Fill/>
+        <PiVideo/>
+        <LuClock3/>
+        <SlLike/>
+        <PiFireBold/>
+        <PiShoppingCart/>
+        <CgMusicNote/>
+        <PiFilmSlateDuotone/>
+        <TbBrandLivewire/>
+        <LuGamepad2/>
+        <FaRegNewspaper/>
+        <GoTrophy/> 
+        <CgBulb/>
+        <PiDressLight/>
+        <FaPodcast/>
+        
+
+  </div>
+  )
+}
+
+export default sidebar
